04d66f71d88b1b596e6d3e28de0237ebe3ff6b10
Srinivasan Mohan <srinivas.mohan@gmail.com>
2013-05-24 00:53:21 -0700

    Added ohai and /envinfo

--- app.rb
+++ app.rb
@@ -2,16 +2,25 @@
 require 'rubygems'
 
 require 'sinatra/base'
-
+require 'ohai'
 class SinnerApp < Sinatra::Base
+
+  configure do
+
+  end
   get '/' do
-    return wrap("<h2>This is a Sinatra test app running on OpenShift</h2><br> Click here for your <a href=\"/ua\">UA/IP info</a>.")
+    return wrap("<h2>This is a Sinatra test app running on OpenShift</h2><br> Click here for your <a href=\"/ua\">UA/IP info</a>.<br>Click here for <a href=\"/envinfo\">Ohai info</a><br>")
   end
 
   get '/ua' do
     wrap("<h2>Your browser: #{request.user_agent}</h2><h2>Your (internet) IP address: #{request.ip}</h2>")
   end
- 
+
+  get '/envinfo' do
+    content_type :json
+    return getfromOhai
+  end 
+  
   def wrap(msg=nil)
   html=<<HTML
 <!doctype html>
@@ -52,5 +61,10 @@ HTML
   return html
   end 
 
+  def getfromOhai
+    thissys=Ohai::System.new
+    thissys.all_plugins
+    return thissys.to_json
+  end
   run! if app_file == $0
 end
